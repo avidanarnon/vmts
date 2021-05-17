2b4c085b2f23e823e894b742bf84368169277fd9
Justin Becker <probitas.software@outlook.com>
2021-05-16 21:08:50 -0600

    wip

--- update.version.js
+++ update.version.js
@@ -3,7 +3,7 @@ var buildVersion = process.argv[2];
 
 const packageJsonOptions = {
   files: ['package.json', './**/package.json'],
-  from: /("version":).*/g,
+  from: /("version": "0.1.0").*/g,
   to: '"version":"' + buildVersion + '",',
   allowEmptyPaths: false
 };
@@ -19,26 +19,12 @@ const configurePublicApiVersion = {
   allowEmptyPaths: false
 };
 
-const configurePeerDependencies = {
-  files: './**/package.json',
-  from: [
-    /.*(\"@eobar\/foundation\":).*/g,
-  ],
-  to: [
-    '\"@eobar/foundation\": \"' + buildVersion + '"'
-  ],
-  allowEmptyPaths: false
-};
-
 try {
   var packageJsonResult = replace.sync(packageJsonOptions);
   console.log('Updated package configuration to version: ' + buildVersion);
   console.log(packageJsonResult);
   var publicApiResult = replace.sync(configurePublicApiVersion);
   console.log('Update public API to version: ' + buildVersion);
-  console.log(publicApiResult);
-  var publicApiResult = replace.sync(configurePeerDependencies);
-  console.log('Update peer dependencies: ' + buildVersion);
   console.log(publicApiResult);
 } catch (error) {
   console.error('Error occurred:', error);
